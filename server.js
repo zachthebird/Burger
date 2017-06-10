ce263a5675038be841c9234933c2aab46c31abd4
Zach Bird <zachthebird@gmail.com>
2017-06-09 21:49:00 -0500

    all working

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.set("view engine", "handlebars");
 var routes = require("./controllers/burgersController.js");
 
 app.use("/", routes);
-
 app.use("/update", routes);
 app.use("/create", routes);
 
