c774ce4f07c677659880f831a549d0e7704f2bd4
Zach Bird <zachthebird@gmail.com>
2017-06-09 21:36:29 -0500

    cleaning up seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,8 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Milk Steak Burger with Jelly Beans', TRUE);
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('Crabby Patty', FALSE);
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('The Baconator', FALSE);
+
 INSERT INTO burgers (burger_name, devoured) VALUES ('The First-Born Burger', TRUE);
 
